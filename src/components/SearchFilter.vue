8f8cdd3d65e6cb8288c3a3e2233ab8d6e8fcc835
Abhinav3499 <3499arya@gmail.com>
2026-01-29 14:52:36 +0530

    Implement filter by difficulty and status

---
+++ src/components/SearchFilter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="search-filter-bar">
+    <select 
+      class="filter-select"
+      :value="store.filterDifficulty"
+      @change="store.setFilterDifficulty($event.target.value)"
+    >
+      <option value="">All Difficulties</option>
+      <option value="Easy">Easy</option>
+      <option value="Medium">Medium</option>
+      <option value="Hard">Hard</option>
+    </select>
+    <select 
+      class="filter-select"
+      :value="store.filterStatus"
+      @change="store.setFilterStatus($event.target.value)"
+    >
+      <option value="">All Status</option>
+      <option value="solved">Solved</option>
+      <option value="unsolved">Unsolved</option>
+      <option value="starred">Starred</option>
+      <option value="revision">Revision</option>
+    </select>
+  </div>
+</template>
+
+<script setup>
+import { useQuestionStore } from '../stores/questionStore'
+
+const store = useQuestionStore()
+</script>
